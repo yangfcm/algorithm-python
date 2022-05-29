aa698b316ad27d9b23fb358496dd195e284c0d48
Fan Yang <yangfcm16@gmail.com>
2022-05-29 21:39:37 +0930

    Implement and test all construct.

--- algo/dynamic/all_construct.py
+++ algo/dynamic/all_construct.py
@@ -12,14 +12,33 @@ def solution1(target, strs):
     if target.startswith(s):
       sub = target[len(s):]
       subconstructs = solution1(sub, strs)
-      targetconstructs = []
       targetconstructs = [[s]+ construct for construct in subconstructs]
-      print(constructs, targetconstructs)
       constructs += targetconstructs
   return constructs
 
-def solution2(target, strs):
-  pass
+def solution2(target, strs, memo = None):
+  if memo is None: memo = {}
+  if target in memo: return memo[target]
+  if target == '': return [[]]
+  constructs = []
+  for s in strs:
+    if target.startswith(s):
+      sub = target[len(s):]
+      subconstructs = solution2(sub, strs, memo)
+      targetconstructs = [[s]+ construct for construct in subconstructs]
+      constructs += targetconstructs
+
+  memo[target] = constructs
+  return constructs
 
 def solution3(target, strs):
-  pass
+  constructs_arr = [[]] * (len(target) + 1)
+  constructs_arr[0] = [[]]
+
+  for i in range(len(target)):
+    for s in strs:
+      if target[i:i+len(s)] == s and i + len(s) <= len(target):
+        newconstruct =(construct + [s] for construct in construct_arr[i])
+        construct_arr[i + len(s)].append(newconstruct)
+  
+  return construct_arr[len(target)]
--- test/dynamic/test_all_construct.py
+++ test/dynamic/test_all_construct.py
@@ -12,3 +12,41 @@ class TestAllConstruct:
         ["abc", "def"],
         ["abcd", "ef"],
       ]
+
+  def test_solution2(self):
+    assert all_construct.solution2("purple", ["purp", "p", "ur", "le", "purpl"]) == \
+     [["purp", "le"], ["p", "ur", "p", "le"]]
+    assert all_construct.solution2("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]) == \
+      [
+        ["ab", "cd", "ef"],
+        ["ab", "c", "def"],
+        ["abc", "def"],
+        ["abcd", "ef"],
+      ]
+    assert all_construct.solution2("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+      "e",
+      "ee",
+      "eee",
+      "eeee",
+      "eeeee",
+      "eeeeee",
+    ]) == []
+
+    def test_solution3(self):
+      assert all_construct.solution3("purple", ["purp", "p", "ur", "le", "purpl"]) == \
+      [["purp", "le"], ["p", "ur", "p", "le"]]
+      assert all_construct.solution3("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]) == \
+        [
+          ["ab", "cd", "ef"],
+          ["ab", "c", "def"],
+          ["abc", "def"],
+          ["abcd", "ef"],
+        ]
+      assert all_construct.solution3("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+      "e",
+      "ee",
+      "eee",
+      "eeee",
+      "eeeee",
+      "eeeeee",
+    ]) == []
