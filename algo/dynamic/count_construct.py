15430bda0193f720ea86ffa1e864bfd51813c524
Fan Yang <yangfcm16@gmail.com>
2022-05-29 20:41:03 +0930

    Implement two count construct solutions.

--- algo/dynamic/count_construct.py
+++ algo/dynamic/count_construct.py
@@ -14,8 +14,26 @@ def solution1(target, strs):
       totalcount += solution1(sub, strs)
   return totalcount
 
-def solution2(target, strs):
-  pass
+def solution2(target, strs, memo = None):
+  if memo is None: memo = {}
+  if target in memo: return memo[target]
+  if target == '': return 1
+  totalcount = 0
+  for s in strs:
+    if target.startswith(s):
+      sub = target[len(s):]
+      totalcount += solution2(sub, strs, memo)
+      memo[target] = totalcount
+  return totalcount
 
 def solution3(target, strs):
-  pass
+  test_arr = [0] * (len(target) + 1)
+  test_arr[0] = 1
+
+  for i in range(len(target)):
+    for s in strs:
+      if target[i:i+len(s)] == s and i + len(s) <= len(target):
+        test_arr[i+len(s)] += test_arr[i]
+  
+  return test_arr[len(target)]
+  
--- test/dynamic/test_count_construct.py
+++ test/dynamic/test_count_construct.py
@@ -7,3 +7,31 @@ class TestCountConstruct:
     assert count_construct.solution1("abcdef", ["ab", "abc", "cd", "def", "abcd"]) == 1
     assert count_construct.solution1("purple", ["purp", "p", "ur", "le", "purpl"]) == 2
     assert count_construct.solution1("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]) == 4
+  
+  def test_solution2(self):
+    assert count_construct.solution2("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]) == 0
+    assert count_construct.solution2("abcdef", ["ab", "abc", "cd", "def", "abcd"]) == 1
+    assert count_construct.solution2("purple", ["purp", "p", "ur", "le", "purpl"]) == 2
+    assert count_construct.solution2("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]) == 4
+    assert count_construct.solution2("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+      "e",
+      "ee",
+      "eee",
+      "eeee",
+      "eeeee",
+      "eeeeee",
+    ]) == 0
+  
+  def test_solution3(self):
+    assert count_construct.solution3("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]) == 0
+    assert count_construct.solution3("abcdef", ["ab", "abc", "cd", "def", "abcd"]) == 1
+    assert count_construct.solution3("purple", ["purp", "p", "ur", "le", "purpl"]) == 2
+    assert count_construct.solution3("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]) == 4
+    assert count_construct.solution3("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+      "e",
+      "ee",
+      "eee",
+      "eeee",
+      "eeeee",
+      "eeeeee",
+    ]) == 0
