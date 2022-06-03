5ee28ca67322fca145d531405879bc319d5df528
Fan Yang <yangfcm16@gmail.com>
2022-06-03 22:20:01 +0930

    Implemented graph and testing.

--- algo/ds/graph/graph.py
+++ algo/ds/graph/graph.py
@@ -8,26 +8,88 @@ class Graph:
     self.__numberOfEdges__ = 0
     self.__numberOfVertex__ = len(vertexList)
     self.__matrix__ = [ [0] * self.__numberOfVertex__ for _ in range(self.__numberOfVertex__)]
-    self.__isVisited__ = [] * self.__numberOfVertex__
-    self.__graphArr__ = []
+    self.__isVisited__ = []  # A list to save which vertex is traversed.
+    self.__traverseList__ = []   # A list to record the traverse path.
   
-  def insertEdge(start, end, weight = 1):
-    pass
+  def insertEdge(self, start, end, weight = 1):
+    if start < 0 or start >= self.__numberOfVertex__: raise ValueError('Start index is out of bound')
+    if end < 0 or end >= self.__numberOfVertex__: raise ValueError('End index is out of bound')
+    if type(weight) != int or weight < 0: raise ValueError('Weight must be a positive integer')
+    if start == end: return
+    if self.__matrix__[start][end] == 0:
+      self.__matrix__[start][end] = weight
+      if self.__directed__ == False:
+        self.__matrix__[end][start] = weight
+    self.__numberOfEdges__ += 1
+    
+  def removeEdge(self, start, end):
+    if start < 0 or start >= self.__numberOfVertex__: raise ValueError('Start index is out of bound')
+    if end < 0 or end >= self.__numberOfVertex__: raise ValueError('End index is out of bound')
+    if start == end: return
+    if self.__matrix__[start][end] != 0:
+      self.__matrix__[start][end] = 0
+      if self.__directed__ == False:
+        self.__matrix__[end][start] = 0
+    self.__numberOfEdges__ -= 1
 
-  def removeEdge(start, end):
-    pass
-
+  # Given the index of a vertex, find its first adjacent vertex
   def getFirstAdjacent(self, v):
-    pass
+    if v < 0 or v >= self.__numberOfVertex__: raise ValueError('Vertex index is out of bound')
+    for i in range(self.__numberOfVertex__):
+      if self.__matrix__[v][i] > 0: return i
+    return -1
 
+  # Given the index of a vertex(v1), find its first adjacent vertex starting from v2
   def getNextAdjacent(self, v1, v2):
-    pass
+    if v1 < 0 or v1 >= self.__numberOfVertex__ or v2 < 0 or v2 >= self.__numberOfVertex__: 
+      raise ValueError('Vertex index is out of bound') 
+    for i in range(v2+1, self.__numberOfVertex__):
+      if self.__matrix__[v1][i] > 0: return i
+    return -1
+  
+  def getValueByIndex(self, index):
+    return self.__vertexList__[index]
 
   def traverseBFS(self):
-    pass
+    self.__traverseList__ = []
+    self.__isVisited__ = [False] * self.__numberOfVertex__
+    for i in range(self.__numberOfVertex__):
+      if self.__isVisited__[i] == False:
+        self.traverseBFSVertex(i)
+    return self.__traverseList__
+
+  def traverseBFSVertex(self, index):
+    queue = []
+    self.__traverseList__.append(self.getValueByIndex(index))
+    self.__isVisited__[index] = True
+    queue.append(index)
+    while len(queue) > 0:
+      u = queue.pop(0)
+      adjacent = self.getFirstAdjacent(u)
+      while adjacent != -1:
+        if self.__isVisited__[adjacent] == False:
+          self.__traverseList__.append(self.getValueByIndex(adjacent))
+          self.__isVisited__[adjacent] = True
+          queue.append(adjacent)
+        adjacent = self.getNextAdjacent(u, adjacent)
 
   def traverseDFS(self):
-    pass
+    self.__traverseList__ = []
+    self.__isVisited__ = [False] * self.__numberOfVertex__
+    for i in range(self.__numberOfVertex__):
+      if self.__isVisited__[i] == False:
+        self.traverseDFSVertex(i)
+    return self.__traverseList__
+
+  def traverseDFSVertex(self, index):
+    vertexList = []
+    self.__traverseList__.append(self.getValueByIndex(index))
+    self.__isVisited__[index] = True
+    w = self.getFirstAdjacent(index)
+    while w >= 0:
+      if self.__isVisited__[w] == False:
+        self.traverseDFSVertex(w)
+      w = self.getNextAdjacent(index, w)
 
   def getDirected(self):
     return self.__directed__
--- test/ds/graph/test_graph.py
+++ test/ds/graph/test_graph.py
@@ -2,6 +2,26 @@ import pytest
 
 from algo.ds.graph.graph import Graph
 
+@pytest.fixture
+def directedGraph():
+  dg = Graph([1,2,3,4,5], True)
+  dg.insertEdge(0, 1)
+  dg.insertEdge(1, 3)
+  dg.insertEdge(2, 3)
+  dg.insertEdge(2, 4)
+  dg.insertEdge(0, 4)
+  dg.insertEdge(4, 0)
+  return dg
+
+@pytest.fixture
+def undirectedGraph():
+  udg = Graph([1,2,3,4,5], False)
+  udg.insertEdge(0, 1)
+  udg.insertEdge(1, 3)
+  udg.insertEdge(2, 3)
+  udg.insertEdge(2, 4)
+  udg.insertEdge(0, 4)
+  return udg
 class TestGraph:
 
   def test_init(self):
@@ -17,3 +37,41 @@ class TestGraph:
       [0,0,0,0,0],
       [0,0,0,0,0]
     ]
+  
+  def test_directed_graph_insert_remove_edge(self):
+    graph = Graph([1,2,3,4,5], True)
+    graph.insertEdge(0, 1)
+    assert graph.getNumberOfEdges() == 1
+    assert graph.getMatrix()[0][1] == 1
+    assert graph.getMatrix()[1][0] == 0
+    graph.insertEdge(2, 4, 9)
+    assert graph.getNumberOfEdges() == 2
+    assert graph.getMatrix()[2][4] == 9
+    assert graph.getMatrix()[4][2] == 0
+    graph.removeEdge(2, 4)
+    assert graph.getNumberOfEdges() == 1
+    assert graph.getMatrix()[2][4] == 0
+    assert graph.getMatrix()[4][2] == 0    
+
+  def test_undirected_graph_insert__remove_edge(self):
+    graph = Graph([1,2,3,4,5], False)
+    graph.insertEdge(0, 1)
+    assert graph.getNumberOfEdges() == 1
+    assert graph.getMatrix()[0][1] == 1
+    assert graph.getMatrix()[1][0] == 1
+    graph.insertEdge(2, 4, 9)
+    assert graph.getNumberOfEdges() == 2
+    assert graph.getMatrix()[2][4] == 9
+    assert graph.getMatrix()[4][2] == 9
+    graph.removeEdge(2, 4)
+    assert graph.getNumberOfEdges() == 1
+    assert graph.getMatrix()[2][4] == 0
+    assert graph.getMatrix()[4][2] == 0
+
+  def test_BFS_traverse(self, directedGraph, undirectedGraph):
+    assert directedGraph.traverseBFS() == [1,2,5,4,3]
+    assert undirectedGraph.traverseBFS() == [1,2,5,4,3]
+
+  def test_DFS_traverse(self, directedGraph, undirectedGraph):
+    assert directedGraph.traverseDFS() == [1,2,4,5,3]
+    assert undirectedGraph.traverseDFS() == [1,2,4,3,5]
