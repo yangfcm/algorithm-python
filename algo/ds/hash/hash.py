5b47acbd6308833ceb1c131923ae3f53c7e0c8ef
Fan Yang <yangfcm16@gmail.com>
2022-06-03 14:32:37 +0930

    Implement and test hash table.

--- algo/ds/hash/hash.py
+++ algo/ds/hash/hash.py
@@ -4,6 +4,7 @@ Implement hash table class
 
 class HashTable:
   def __init__(self, size = 8):
+    if size <= 0: raise ValueError('Size must be a positive integer')
     self.table = [[]] * size
     self.count = 0
     self.size =size
@@ -15,7 +16,7 @@ class HashTable:
     index = self.__hash__(key)
     bucket = self.table[index]
 
-    for i, node in enumerate(bucket, start = 0):
+    for i, node in enumerate(bucket, start = 0):  # See if there's conflicting key. If there is, overwrite it.
       if node[0] == key:
         bucket[i] = (key, value)
         return value
@@ -24,19 +25,34 @@ class HashTable:
     self.count += 1
 
     if(self.count > self.size * .75):
-      self.resize(self.size * 2)
+      self.__resize__(self.size * 2)
     return value
 
   def get(self, key):
     index = self.__hash__(key)
     bucket = self.table[index]
-    if len(bucket) == 0: return None
     for node in bucket:
       if node[0] == key: return node[1]
     return None
 
   def remove(self, key):
-    pass
+    index = self.__hash__(key)
+    bucket = self.table[index]
+    for i, node in enumerate(bucket, start=0):
+      if node[0] == key:
+        bucket.pop(i)
+        self.count -= 1
+        if self.count < self.size * .25:
+          self.__resize__(self.size // 2)
+        return node[1]
+    return None
 
-  def resize(self, newSize):
-    pass
+  def __resize__(self, newSize):
+    oldTable = self.table
+    self.size = newSize
+    self.count = 0
+    self.table = [[]] * newSize
+
+    for bucket in oldTable:
+      for node in bucket:
+        self.put(node[0], node[1])
--- test/ds/hash/test_hash.py
+++ test/ds/hash/test_hash.py
@@ -22,3 +22,33 @@ class TestHashTable:
     assert t.get('age') == 32
     assert t.get('hobbies') == ['cooking', 'fishing', 'camping']
     assert t.get('unknown_key') == None
+
+  def test_remove(self):
+    t = HashTable()
+    t.put('a', 1)
+    t.put('b', 2)
+    t.put('c', 3)
+    assert t.remove('a') == 1
+    assert t.remove('b') == 2
+    assert t.get('a') == None
+    assert t.get('b') == None
+    assert t.remove('b') == None
+    assert t.remove('d') == None
+
+  def test_resize_expansion(self):
+    t = HashTable(2)
+    t.put('a', 1)
+    assert t.size == 2
+    t.put('b', 2) # Now resize to 4.
+    assert t.size == 4
+    t.put('c', 3)
+    assert t.get('a') == 1
+    assert t.get('b') == 2
+    assert t.get('c') == 3
+
+    t.remove('a')
+    t.remove('b')
+    print(t.count)
+    assert t.size == 4
+    t.remove('c') # Now resize to 2.
+    assert t.size == 2
