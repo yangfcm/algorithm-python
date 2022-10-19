183d718e36849a9ce1c9490c27c00d70daf34689
Fan Yang <yangfcm16@gmail.com>
2022-10-19 17:20:24 +1030

    Add comment for solution.

--- algo/dynamic/common_child.py
+++ algo/dynamic/common_child.py
@@ -3,6 +3,7 @@
   Given two strings of equal length, find out the longest string that can be constructed such that it is a child of both.
   Example: s1 = 'ABCD', s2 = 'ABDC'. These two strings have two children with maximum length 3.
   They are 'ABC' and 'ABD'. They can be formed by eliminating either D or C from both strings.
+  Explanation: https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
 '''
 def solution(s1, s2):
   table = [ [0] * (len(s2) + 1) for i in range(len(s1) + 1)]
